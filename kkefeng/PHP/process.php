b1fb5d6359b594fd26f79400bd2da5172af3374b
“Kkefeng” <luffy7channel@gmail.com>
2020-07-21 21:44:26 +0800

    Add PHP

---
+++ kkefeng/PHP/process.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Why you are here</title>
+<link rel="stylesheet" href="phpstyle.css">
+</head>
+<body>
+
+<?php
+$name = $_POST ["name"];
+
+	echo " development purpose";
+?>
+
+
+</body>
+</html>
