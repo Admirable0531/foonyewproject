09652ab73cd1d50b48b6e945086d1441d7a0e818
Admirable <blazerod123480@gmail.com>
2020-07-30 23:49:07 +0800

    sphere and added longer timeout timer

--- test/js/index.js
+++ test/js/index.js
@@ -5,19 +5,19 @@ window.onload = function() {
 
 function clearAll() {
 	for (var i=1; i<=20; i++) {
-	document.querySelector("#box" + i).setAttribute('scale', '0 0 0');
+	document.querySelector("#box" + i).setAttribute('radius', '0.001');
 	}
 	for (var i=1; i<=19; i++) {
 	var value = i + 1001;
-	document.querySelector("#box" + value).setAttribute('scale', '0 0 0');
+	document.querySelector("#box" + value).setAttribute('radius', '0.001');
 	}
 }
 
 function setscene(scene) {
 	document.querySelector('a-sky').setAttribute('src', '#' + scene);
-	document.querySelector("#box" + scene).setAttribute('scale', '1 1 1');
+	document.querySelector("#box" + scene).setAttribute('radius', '1');
 	if((scene+1000) != 1001) {
-	document.querySelector("#box" + (scene+1000)).setAttribute('scale', '1 1 1');
+	document.querySelector("#box" + (scene+1000)).setAttribute('radius', '1');
 } else {	}
 }
 
