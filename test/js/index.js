764436a347563bc63ea0b0bd2492fd7cb4b95eab
yanxun3737 <hoyanxun@gmail.com>
2020-07-30 09:33:32 +0800

    no message

--- test/js/index.js
+++ test/js/index.js
@@ -29,7 +29,11 @@ AFRAME.registerComponent('cursor-listener1', {
 AFRAME.registerComponent('cursor-listener2', {
 	init: function () {
 		document.getElementById('box2').addEventListener('click', function () {
+<<<<<<< HEAD
 			if (document.querySelector("a-sky").getAttribute('src') == "2") {
+=======
+			if (document.querySelector("a-sky").getAttribute('src') == "#2") {
+>>>>>>> 2a6993c156e8d0a7f689345a803666bf704eb406
 				console.log("plz say it works for 2");
 				clearAll();
 				setscene(3);
@@ -42,10 +46,14 @@ AFRAME.registerComponent('cursor-listener2', {
 AFRAME.registerComponent('cursor-listener3', {
 	init: function () {
 		document.getElementById('box3').addEventListener('click', function () {
+<<<<<<< HEAD
 			if (document.querySelector("a-sky").getAttribute('src') == "3") {
+=======
+			if (document.querySelector("a-sky").getAttribute('src') == "#3") {
+>>>>>>> 2a6993c156e8d0a7f689345a803666bf704eb406
 				console.log("plz say it works for 3");
 				clearAll();
-				scene4();
+				setscene(4);
 			} else {	}
 		});
 
@@ -55,10 +63,14 @@ AFRAME.registerComponent('cursor-listener3', {
 AFRAME.registerComponent('cursor-listener4', {
 	init: function () {
 		document.getElementById('box4').addEventListener('click', function () {
+<<<<<<< HEAD
 			if (document.querySelector("a-sky").getAttribute('src') == "4") {
+=======
+			if (document.querySelector("a-sky").getAttribute('src') == "#4") {
+>>>>>>> 2a6993c156e8d0a7f689345a803666bf704eb406
 				console.log("plz say it works for 4");
 				clearAll();
-				scene5();
+				setscene(5);
 			} else {	}
 		});
 
@@ -68,7 +80,11 @@ AFRAME.registerComponent('cursor-listener4', {
 AFRAME.registerComponent('cursor-listener5', {
 	init: function () {
 		document.getElementById('box5').addEventListener('click', function () {
+<<<<<<< HEAD
 			if (document.querySelector("a-sky").getAttribute('src') == "5") {
+=======
+			if (document.querySelector("a-sky").getAttribute('src') == "#5") {
+>>>>>>> 2a6993c156e8d0a7f689345a803666bf704eb406
 				console.log("plz say it works for 5");
 				clearAll();
 				scene1();
