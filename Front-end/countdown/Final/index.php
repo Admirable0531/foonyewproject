34289428b6a3c0ee9d1945c0de0b59a6ab8be244
Admin-PC\Admin <luffy7channel@gmail.com>
2020-07-30 14:41:02 +0800

    Edit

--- Front-end/countdown/Final/index.php
+++ Front-end/countdown/Final/index.php
@@ -1,10 +1,11 @@
-<!doctype html>
+	<!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">	
 		<title>107校庆倒数</title>
 		<link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
 	</head>
 	<body>
 		<div class="bg">
