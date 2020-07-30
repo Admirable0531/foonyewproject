cf829af0a35ab7239fb1deda5e60bbc1f9762036
Admirable <blazerod123480@gmail.com>
2020-07-30 23:06:57 +0800

    added backwards

--- test/js/index.js
+++ test/js/index.js
@@ -1,20 +1,24 @@
 window.onload = function() {
 	clearAll();
 	setscene(1);
-	console.log("ready");
 }
 
 function clearAll() {
 	for (var i=1; i<=20; i++) {
-	var scene = ".scene" + i;
 	document.querySelector("#box" + i).setAttribute('scale', '0 0 0');
+	}
+	for (var i=1; i<=19; i++) {
+	var value = i + 1001;
+	document.querySelector("#box" + value).setAttribute('scale', '0 0 0');
 	}
 }
 
 function setscene(scene) {
-	var s = ".scene" + scene;
-	document.querySelector("#box" + scene).setAttribute('scale', '1 1 1');
 	document.querySelector('a-sky').setAttribute('src', '#' + scene);
+	document.querySelector("#box" + scene).setAttribute('scale', '1 1 1');
+	if((scene+1000) != 1001) {
+	document.querySelector("#box" + (scene+1000)).setAttribute('scale', '1 1 1');
+} else {	}
 }
 
 function run(s) {
@@ -23,4 +27,12 @@ function run(s) {
 		setscene(s+1);
 	}
 	console.log("this is scene " + (s+1))
+}
+
+function runback(s) {
+	clearAll();
+	if ("#" + (s+1) == document.getElementById('background').getAttribute('src')) {
+		setscene(s);
+	}
+	console.log("this is scene " + s);
 }
