da8e9711b664d2a1bd645114b3d91fef669a47ef
kkefeng <luffy7channel@gmail.com>
2020-08-01 12:03:01 +0800

    Update Reg v2

--- Front-end/regform v2/tnc.js
+++ Front-end/regform v2/tnc.js
@@ -1,19 +1,12 @@
-window.onload = function(){ 
-	document.getElementById("submit").disabled = true;
+//JavaScript function that enables or disables a submit button depending
+//on whether a checkbox has been ticked or not.
+function terms_changed(termsCheckBox){
+    //If the checkbox has been checked
+    if(termsCheckBox.checked){
+        //Set the disabled property to FALSE and enable the button.
+        document.getElementById("submit_button").disabled = false;
+    } else{
+        //Otherwise, disable the submit button.
+        document.getElementById("submit_button").disabled = true;
+    }
 }
-
-
-function disableSubmit() {
-  document.getElementById("submit").disabled = true;
- }
-
-  function activateButton(element) {
-
-      if(element.checked) {
-        document.getElementById("submit").disabled = false;
-       }
-       else  {
-        document.getElementById("submit").disabled = true;
-      }
-
-  }
