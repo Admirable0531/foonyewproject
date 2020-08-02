f02b28f705e64be5e9a3f817bf279ebfa8e23e13
“Kkefeng” <luffy7channel@gmail.com>
2020-08-02 16:06:13 +0800

    Move trashy folder and works to Trash folder

---
+++ Front-end/countdown/Trash/v2-1/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">	
+		<title>107校庆倒数</title>
+		<link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	</head>
+	<body>
+		<div class="bg">
+			<div class="middle">
+
+			<h1>校庆倒数</h1>
+			
+
+			<hr>
+
+			<p id = "timer" style="font-size: 30px"></p>
+
+			</div>
+		</div>
+	</body>
+		
+<script type="text/javascript" src="timer.js" async></script>
+</html>
+
+<!-- FoonYewProject-Front-end -->
+
+
+
+
+
+
+
+
+
+
+
