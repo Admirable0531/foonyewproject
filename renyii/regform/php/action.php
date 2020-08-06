ebee19998589c4a55694d4438873461637f8d8f2
Ren Yi <jb170352@foonyew.edu.my>
2020-08-06 09:36:24 +0800

    update index / create random_id in regform

--- renyii/regform/php/action.php
+++ renyii/regform/php/action.php
@@ -13,16 +13,34 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+function random_str(
+    int $length = 64,
+    string $keyspace = '0123456789'
+): string {
+    if ($length < 1) {
+        throw new \RangeException("Length must be a positive integer");
+    }
+    $pieces = [];
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $pieces []= $keyspace[random_int(0, $max)];
+    }
+    return implode('', $pieces);
+}
+
+$random_id = random_str(10);
 $student_id = $_POST['student_id'];
 $name = $_POST['name'];
 $contact = $_POST['contact'];
 $school = $_POST['school'];
 $type = $_POST['type'];
 
-$sql = "INSERT INTO mystudents (contact,student_id,name,school,type) VALUES ('$contact','$student_id','$name','$school','$type')";
+$sql = "INSERT INTO mystudents (random_id,student_id,name,contact,school,type) VALUES ('$random_id','$student_id','$name','$contact','$school','$type')";
 
 //创建会话数据
 if(empty($_SESSION)){
+	
+    $_SESSION['random_id'] = "$random_id";
     $_SESSION['name'] = "$name";
     $_SESSION['id'] = "$student_id";
     echo "<p>您的资料已经提交成功。</p>";
@@ -42,5 +60,5 @@ if ($conn->query($sql) === TRUE) {
 
 session_write_close(); 
 $conn->close();
-header('Location: http://www.google.com');
+//header('Location: http://www.google.com');
 ?>
